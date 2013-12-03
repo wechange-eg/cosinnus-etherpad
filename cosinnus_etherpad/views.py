888ae0f6c24d9b9264712d328724cd6a2a0aa98a
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-12-03 12:11:18 +0000

    Improved domain handling for third-party cookies

--- cosinnus_etherpad/views.py
+++ cosinnus_etherpad/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+try:
+    from urllib import parse as urlparse
+except ImportError:
+    from urlparse import urlparse
+import logging
+
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, RedirectView
 from django.views.generic.list import ListView
@@ -12,6 +18,7 @@ from cosinnus.views.mixins.group import (RequireGroupMixin, FilterGroupMixin,
     GroupFormKwargsMixin)
 from cosinnus.views.mixins.tagged import TaggedListMixin
 
+from cosinnus_etherpad.conf import settings
 from cosinnus_etherpad.models import Etherpad
 from cosinnus_etherpad.forms import EtherpadForm
 
@@ -26,15 +33,36 @@ class EtherpadIndexView(RequireGroupMixin, RedirectView):
 class EtherpadView(RequireGroupMixin, FilterGroupMixin, DetailView):
     model = Etherpad
 
+    def _get_cookie_domain(self):
+        domain = urlparse(settings.COSINNUS_ETHERPAD_BASE_URL).netloc
+
+        # strip the port (if exists)
+        domain = domain.split(':')[0]
+
+        # strip the hostname
+        split_domain = domain.split('.')
+        # only if we have at least 2 dots use the split domain
+        # http://curl.haxx.se/rfc/cookie_spec.html
+        if len(split_domain) > 2:
+            domain = '.' + '.'.join(split_domain[1:])
+        else:
+            domain = None
+        return domain
+
+
     def render_to_response(self, context, **response_kwargs):
         response = super(EtherpadView, self).render_to_response(
             context, **response_kwargs)
 
+        # set cross-domain session cookie for etherpad app
         etherpad = context['etherpad']
         user_session_id = etherpad.get_user_session_id(self.request.user)
-        # set cross-domain session cookie for etherpad app
-        response.set_cookie('sessionID', user_session_id,
-                            domain='.ideenhochdrei.org')
+        domain = self._get_cookie_domain()
+        if domain:
+            server_name = self.request.META['SERVER_NAME']
+            if domain not in server_name:
+                logging.warning('SERVER_NAME %s and cookie domain %s don\'t match. Setting a third-party cookie might not work!' % (server_name, domain))
+        response.set_cookie('sessionID', user_session_id, domain=domain)
 
         return response
 
