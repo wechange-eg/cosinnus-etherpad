2dde4c88a7765d56780105fb09680991762d9b0a
Sebastian Henschel <sh@sinnwerkstatt.com>
2014-01-29 15:59:45 +0100

    Changed list/detail view templates to show add/edit only to group members

--- tests/view_tests/test_list.py
+++ tests/view_tests/test_list.py
@@ -9,10 +9,21 @@ from tests.view_tests.base import ViewTestCase
 
 class ListTest(ViewTestCase):
 
-    def test_list(self):
+    def test_list_not_logged_in(self):
         """
-        Should return 200 and contain URL to add a pad
+        Should return 200 if not logged in
         """
+        kwargs = {'group': self.group.slug}
+        url = reverse('cosinnus:etherpad:list', kwargs=kwargs)
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_logged_in_admin(self):
+        """
+        Should return 200 and contain URL to add a pad if admin
+        """
+        self.client.login(username=self.credential, password=self.credential)
         kwargs = {'group': self.group.slug}
         url = reverse('cosinnus:etherpad:list', kwargs=kwargs)
         response = self.client.get(url)
@@ -47,7 +58,7 @@ class ListTest(ViewTestCase):
 
         kwargs = {'group': self.group.slug, 'slug': pad.slug}
         self.assertIn(
-            reverse('cosinnus:etherpad:pad-edit', kwargs=kwargs),
+            reverse('cosinnus:etherpad:pad-detail', kwargs=kwargs),
             str(response.content))  # type byte in Python3.3
 
         # be nice to remote server and delete pad also there
