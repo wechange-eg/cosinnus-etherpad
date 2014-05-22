6f526770b43d7925faa30044e8599a6903ab991f
Markus Holtermann <info@markusholtermann.eu>
2014-05-22 14:03:53 +0200

    Cleanup

--- cosinnus_etherpad/cosinnus_app.py
+++ cosinnus_etherpad/cosinnus_app.py
@@ -9,9 +9,6 @@ def register():
     from cosinnus.core.registries import (app_registry, url_registry,
         widget_registry)
 
-    from cosinnus_etherpad.urls import (cosinnus_group_patterns,
-        cosinnus_root_patterns)
-
     app_registry.register('cosinnus_etherpad', 'etherpad', _('Etherpads'))
     url_registry.register_urlconf('cosinnus_etherpad', 'cosinnus_etherpad.urls')
     widget_registry.register('etherpad', 'cosinnus_etherpad.dashboard.Latest')
