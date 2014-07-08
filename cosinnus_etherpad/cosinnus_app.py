d3f18ccd48ada93973a2897ca07891aa1a2b230d
Sascha Narr <saschanarr@googlemail.com>
2014-07-08 19:42:27 +0200

    Made Etherpads attachable. Pads can now be attached.

--- cosinnus_etherpad/cosinnus_app.py
+++ cosinnus_etherpad/cosinnus_app.py
@@ -6,9 +6,11 @@ def register():
     # Import here to prevent import side effects
     from django.utils.translation import ugettext_lazy as _
 
-    from cosinnus.core.registries import (app_registry, url_registry,
-        widget_registry)
+    from cosinnus.core.registries import (app_registry, attached_object_registry, 
+        url_registry, widget_registry)
 
-    app_registry.register('cosinnus_etherpad', 'etherpad', _('Etherpads'))
+    app_registry.register('cosinnus_etherpad', 'etherpad', _('Etherpads'))   
+    attached_object_registry.register('cosinnus_etherpad.Etherpad',
+                             'cosinnus_etherpad.utils.renderer.EtherpadRenderer')
     url_registry.register_urlconf('cosinnus_etherpad', 'cosinnus_etherpad.urls')
     widget_registry.register('etherpad', 'cosinnus_etherpad.dashboard.Latest')
---
+++ cosinnus_etherpad/utils/renderer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 08.07.2014
+
+@author: Sascha Narr
+"""
+from __future__ import unicode_literals
+
+from cosinnus.utils.renderer import BaseRenderer
+
+
+class EtherpadRenderer(BaseRenderer):
+
+    template = 'cosinnus_etherpad/attached_etherpads.html'
+
+    @classmethod
+    def render(cls, context, myobjs):
+        return super(EtherpadRenderer, cls).render(context, etherpads=myobjs)
