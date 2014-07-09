09708e2a70482cc168fa7d007bd0d41b6b2add8f
Sascha Narr <saschanarr@googlemail.com>
2014-07-09 20:00:49 +0200

    Adding API directed widget data reloading

--- cosinnus_etherpad/dashboard.py
+++ cosinnus_etherpad/dashboard.py
@@ -24,15 +24,15 @@ class Latest(DashboardWidget):
     user_model_attr = None  # No filtering on user page
     widget_name = 'latest'
 
-    def get_data(self):
+    def get_data(self, offset=0):
         count = int(self.config['amount'])
         qs = self.get_queryset().select_related('group').order_by('-created').filter(is_container=False)
         if count != 0:
-            qs = qs[:count]
+            qs = qs[offset:offset+count]
             
         data = {
             'rows': qs,
             'no_data': _('No etherpads'),
             'group': self.config.group,
         }
-        return render_to_string('cosinnus_etherpad/widgets/latest.html', data)
+        return (render_to_string('cosinnus_etherpad/widgets/latest.html', data), len(qs))
