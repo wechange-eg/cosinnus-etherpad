0f21dd84a8b45d35a87b969faaddeb662b6014de
Sascha Narr <saschanarr@googlemail.com>
2014-08-05 17:08:57 +0200

    Finalized etherpad filters and translation

--- cosinnus_etherpad/filters.py
+++ cosinnus_etherpad/filters.py
@@ -3,21 +3,24 @@ Created on 05.08.2014
 
 @author: Sascha
 '''
+from django.utils.translation import ugettext_lazy as _
+
 from cosinnus.views.mixins.filters import CosinnusFilterSet
-from cosinnus.forms.filters import AllObjectsFilter, DropdownChoiceWidget,\
-    SelectUserWidget
-import django_filters
+from cosinnus.forms.filters import AllObjectsFilter, SelectCreatorWidget
 from cosinnus_etherpad.models import Etherpad
 
+
 class EtherpadFilter(CosinnusFilterSet):
-    #title = django_filters.CharFilter(label=_('Description'))
-    creator = AllObjectsFilter(label=_('Creator'), widget=SelectUserWidget)
-    created = django_filters.DateRangeFilter(label=_('Date created'), widget=DropdownChoiceWidget)
+    creator = AllObjectsFilter(label=_('Created By'), widget=SelectCreatorWidget)
     
     class Meta:
         model = Etherpad
-        fields = ['creator', 'created'] #creator__username #'title', 
-        order_by = ['-created', 'title']
+        fields = ['creator']
+        order_by = (
+            ('-created', _('Newest Pads')),
+            ('title', _('Title')),
+        )
     
     def get_order_by(self, order_value):
         return super(EtherpadFilter, self).get_order_by(order_value)
+    
