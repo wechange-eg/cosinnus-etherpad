d13e545576258540476b00b4db210fd7af87fbc0
Sascha Narr <saschanarr@googlemail.com>
2014-10-07 11:37:35 +0200

    Fixed missing trailing slash in url

--- cosinnus_etherpad/urls.py
+++ cosinnus_etherpad/urls.py
@@ -7,7 +7,7 @@ cosinnus_group_patterns = patterns('cosinnus_etherpad.views',
     url(r'^$', 'index_view', name='index'),
     url(r'^list/$', 'pad_hybrid_list_view', name='list'),
     url(r'^list/(?P<slug>[^/]+)/$', 'pad_hybrid_list_view', name='list'),
-    url(r'^(?P<slug>[^/]+)/edit$', 'pad_edit_view', name='pad-edit'),
+    url(r'^(?P<slug>[^/]+)/edit/$', 'pad_edit_view', name='pad-edit'),
     url(r'^(?P<slug>[^/]+)/$', 'pad_detail_view', name='pad-detail'),
     
     #url(r'^list/(?P<tag>[^/]+)/$', 'pad_hybrid_list_view', name='list-filtered'),
