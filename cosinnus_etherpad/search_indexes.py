4ab9272e309f3a7b04b978414e20a1d101586f78
Sascha Narr <saschanarr@googlemail.com>
2015-09-08 12:29:01 +0200

    Add search indexes filter for BaseHierarchicalTaggableObjects

--- cosinnus_etherpad/search_indexes.py
+++ cosinnus_etherpad/search_indexes.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 
 from haystack import indexes
 
-from cosinnus.utils.search import BaseTaggableObjectIndex
+from cosinnus.utils.search import BaseHierarchicalTaggableObjectIndex
 
 from cosinnus_etherpad.models import Etherpad
 
 
-class EtherpadIndex(BaseTaggableObjectIndex, indexes.Indexable):
+class EtherpadIndex(BaseHierarchicalTaggableObjectIndex, indexes.Indexable):
     description = indexes.CharField(model_attr='description')
     pad_id = indexes.CharField(model_attr='pad_id', indexed=False)
 
