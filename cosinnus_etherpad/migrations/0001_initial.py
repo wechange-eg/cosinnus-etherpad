7846065b72b6e761e495fd142f748dd313aaaa32
Sascha Narr <saschanarr@googlemail.com>
2015-11-04 17:37:47 +0100

    Upgrade south migrations.

--- cosinnus_etherpad/migrations/0001_initial.py
+++ cosinnus_etherpad/migrations/0001_initial.py
@@ -1,72 +1,39 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Etherpad'
-        db.create_table(u'cosinnus_etherpad_etherpad', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('media_tag', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cosinnus.TagObject'], unique=True, null=True, on_delete=models.PROTECT, blank=True)),
-            ('group', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'cosinnus_etherpad_etherpad_set', on_delete=models.PROTECT, to=orm['auth.Group'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=55)),
-            ('pad_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal(u'cosinnus_etherpad', ['Etherpad'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('cosinnus', '0001_initial'),
+    ]
 
-        # Adding unique constraint on 'Etherpad', fields ['group', 'slug']
-        db.create_unique(u'cosinnus_etherpad_etherpad', ['group_id', 'slug'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'Etherpad', fields ['group', 'slug']
-        db.delete_unique(u'cosinnus_etherpad_etherpad', ['group_id', 'slug'])
-
-        # Deleting model 'Etherpad'
-        db.delete_table(u'cosinnus_etherpad_etherpad')
-
-
-    models = {
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'cosinnus.tagobject': {
-            'Meta': {'object_name': 'TagObject'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'place': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        u'cosinnus_etherpad.etherpad': {
-            'Meta': {'unique_together': "((u'group', u'slug'),)", 'object_name': 'Etherpad'},
-            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'cosinnus_etherpad_etherpad_set'", 'on_delete': 'models.PROTECT', 'to': u"orm['auth.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'media_tag': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.TagObject']", 'unique': 'True', 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
-            'pad_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '55'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['cosinnus_etherpad']
+    operations = [
+        migrations.CreateModel(
+            name='Etherpad',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='Title')),
+                ('slug', models.SlugField(max_length=55, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='Created')),
+                ('is_container', models.BooleanField(default=False)),
+                ('special_type', models.CharField(default=None, editable=False, max_length=8, blank=True, help_text='A special folder appears differently on the site and cannot be deleted by users', null=True)),
+                ('path', models.CharField(default='/', max_length=100, verbose_name='Path')),
+                ('pad_id', models.CharField(max_length=255)),
+                ('description', models.TextField(verbose_name='Description', blank=True)),
+                ('group_mapper', models.CharField(max_length=255, null=True, blank=True)),
+                ('attached_objects', models.ManyToManyField(to='cosinnus.AttachedObject', null=True, blank=True)),
+                ('creator', models.ForeignKey(related_name='cosinnus_etherpad_etherpad_set', verbose_name='Creator', to=settings.AUTH_USER_MODEL, null=True)),
+                ('group', models.ForeignKey(related_name='cosinnus_etherpad_etherpad_set', verbose_name='Group', to='cosinnus.CosinnusGroup')),
+            ],
+            options={
+                'abstract': False,
+                'verbose_name': 'Etherpad',
+                'verbose_name_plural': 'Etherpads',
+            },
+        ),
+    ]
